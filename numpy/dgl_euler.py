ae8e63c74c988bb9b46985f5a7b88d57d596ebcd
zumbrseb <zumbrseb@students.zhaw.ch>
2023-06-19 10:07:04 +0200

    add system py

--- numpy/dgl_euler.py
+++ numpy/dgl_euler.py
@@ -1,19 +1,21 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 #######################
 # Definitionen        #
 #######################
 
-a = -1.5
-b = 1.5
-n = 5
+a = 0
+b = 6
+n = 30
 
 y0 = 0
-f = lambda x, y: x**2 + 0.1 * y
+def f(x, y): return 0.1*y+np.sin(2*x)
 
 #######################
 # Euler Verfahren     #
 #######################
+
 
 step_size = (b - a) / n
 
@@ -25,3 +27,23 @@ for i in range(n):
     y[i + 1] = y[i] + step_size * f(x[i], y[i])
 
 print(y)
+
+
+x_min = a
+x_max = b
+y_min = -1
+y_max = 3
+
+count = 30
+
+plt.plot(x, y, label="euler")
+
+mesh_x, mesh_y = np.meshgrid(
+    np.linspace(x_min, x_max, count),
+    np.linspace(y_min, y_max, count)
+)
+
+plt.quiver(mesh_x, mesh_y, 1, f(mesh_x, mesh_y))
+
+plt.legend()
+plt.show()
